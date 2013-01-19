59b3fbdf78e1d07bedb63c22450e4acfe3d49290
Rafael Lima <contato@rafael.adm.br>
2013-01-19 14:46:18 -0200

    update ActiveResource to handle Status 429 Too Many Requests

--- lib/ActiveResource.php
+++ lib/ActiveResource.php
@@ -41,7 +41,8 @@
  * ?>
  *
  * @author John Luxford <lux@companymachine.com>
- * @version 0.14 beta
+ * @author Rafael Lima - http://rafael.adm.br
+ * @version 0.15 beta
  * @license http://opensource.org/licenses/lgpl-2.1.php
  */
 class ActiveResource {
@@ -635,11 +636,24 @@ class ActiveResource {
 		}
 		$res = curl_exec ($ch);
 
-		// Check HTTP status code for denied access
 		$http_code = curl_getinfo ($ch, CURLINFO_HTTP_CODE);
+
+		// Check HTTP status code for denied access
 		if ($http_code == 401) {
 			$this->errno = $http_code;
 			$this->error = "HTTP Basic: Access denied.";
+			curl_close ($ch);
+			return false;
+		}
+
+		// Check HTTP status code for rate limit
+		if ($http_code == 429) {
+			if (preg_match ('/Retry-After: ([0-9]+)/', $res, $retry_after)) {
+			  sleep(intval($retry_after[1]));
+			  return $this->_fetch ($url, $method, $params);
+			}
+			$this->errno = $http_code;
+			$this->error = "Too Many Requests";
 			curl_close ($ch);
 			return false;
 		}
