7e1b30a8785c7fd63674be3290da88182de718c1
Kivanio Barbosa <kivanio@gmail.com>
2014-09-19 23:45:42 -0400

    add subscription

--- lib/CobreGratis.php
+++ lib/CobreGratis.php
@@ -4,5 +4,6 @@ require_once ('ActiveResource.php');
 require_once ('cobregratis/Base.php');
 require_once ('cobregratis/BankBillet.php');
 require_once ('cobregratis/Customer.php');
+require_once ('cobregratis/BankBilletSubscription.php');
 
 ?>
